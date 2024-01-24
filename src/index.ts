fc79a1b59b57c93b6cf8845342a77c9681e20a95
arthurbomfim <arthursantos575@gmail.com>
2024-01-24 11:48:44 -0300

    configuração de edição de codigo

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-import { server } from "./server/server";
+import { server } from './server/server';
 
-server.listen(3000, () => console.log('Servidor rodando na porta 3000'))
+server.listen(3000, () => console.log('Servidor rodando na porta 3000'));
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,12 +1,12 @@
-import express from "express";
-import { Request, Response } from "express";
+import express from 'express';
+import { Request, Response } from 'express';
 const server = express();
 
 
-server.get('/', (req: Request, res: Response) => {
 
-  return res.send('Olá, DEV!')
-})
+server.get('/', (req: Request, res: Response) => {
+  return res.send('Olá, DEV!');
+});
 
 
 export { server };
