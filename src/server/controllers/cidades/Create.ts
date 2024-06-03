caaa10b250e3da5d6fa84fbfed3cce8f1fdbe7e9
arthurbomfim <arthursantos575@gmail.com>
2024-06-03 12:03:25 -0300

    feat: add library yup and body validation

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -1,14 +1,31 @@
 import { Request, Response } from 'express';
-
+import { StatusCodes } from 'http-status-codes';
+import * as yup from 'yup';
 
 interface ICidade {
- nome: string,
+ nome: string
 }
 
-export const create = (req: Request<{}, {}, ICidade>, res: Response) => {
+const bodyValidation: yup.ObjectSchema<ICidade> = yup.object().shape({
+ nome: yup.string().required().min(3),
+});
 
+export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
+ let validatedData: ICidade | undefined = undefined;
 
- console.log(req.body.nome);
+ try {
+  validatedData = await bodyValidation.validate(req.body);
+ } catch (error) {
+  const yupError = error as yup.ValidationError;
+
+  return res.status(StatusCodes.BAD_REQUEST).json({
+   errors: {
+    default: yupError.message,
+   }
+  });
+ }
+
+ console.log(validatedData);
 
 
  return res.send('Create!');
