1ba0cd9a6fd65f6cc051c776cadb0c89c949939a
arthurbomfim <arthursantos575@gmail.com>
2024-06-04 18:06:02 -0300

    feat: add errors and translation for pt-br

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -3,26 +3,30 @@ import { StatusCodes } from 'http-status-codes';
 import * as yup from 'yup';
 
 interface ICidade {
- nome: string
+ nome: string,
+ estado: string
 }
 
 const bodyValidation: yup.ObjectSchema<ICidade> = yup.object().shape({
  nome: yup.string().required().min(3),
+ estado: yup.string().required().min(3)
 });
 
 export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
  let validatedData: ICidade | undefined = undefined;
 
  try {
-  validatedData = await bodyValidation.validate(req.body);
- } catch (error) {
-  const yupError = error as yup.ValidationError;
+  validatedData = await bodyValidation.validate(req.body, { abortEarly: false });
+ } catch (err) {
+  const yupError = err as yup.ValidationError;
+  const errors: Record<string, string> = {};
 
-  return res.status(StatusCodes.BAD_REQUEST).json({
-   errors: {
-    default: yupError.message,
-   }
+  yupError.inner.forEach(error => {
+   if (!error.path) return;
+   errors[error.path] = error.message;
   });
+
+  return res.status(StatusCodes.BAD_REQUEST).json({ errors });
  }
 
  console.log(validatedData);
