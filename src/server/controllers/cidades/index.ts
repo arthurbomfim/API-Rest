1008af34111639437234ce0a936fed068ee6bfe1
arthurbomfim <arthursantos575@gmail.com>
2024-06-17 17:10:00 -0300

    feat: add methods delete, update and getById from cidades

--- src/server/controllers/cidades/index.ts
+++ src/server/controllers/cidades/index.ts
@@ -1,9 +1,15 @@
 import * as create from './Create';
 import * as getAll from './GetAll';
+import * as deleteById from './DeleteById';
+import * as getById from './GetById';
+import * as updateById from './UpdateById';
 
 
 export const cidadesController = {
  ...create,
- ...getAll
+ ...getAll,
+ ...deleteById,
+ ...getById,
+ ...updateById
 };
 
--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -9,7 +9,10 @@ router.get('/', (_, res) => {
 });
 
 router.get('/cidades', cidadesController.getAllValidation, cidadesController.getAll);
+router.get('/cidades/:id', cidadesController.getByIdValidation, cidadesController.getById);
 router.post('/cidades', cidadesController.createValidation, cidadesController.create);
+router.put('/cidades/:id', cidadesController.updateByIdValidation, cidadesController.updateById);
+router.delete('/cidades/:id', cidadesController.deleteByIdValidation, cidadesController.deleteById);
 
 
 export { router };
