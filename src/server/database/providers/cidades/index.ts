b6f053be97f1f1877e79d52cd1348ce4e26b576e
arthurbomfim <arthursantos575@gmail.com>
2024-07-04 15:24:49 -0300

    feat: add provider for cidade

---
+++ src/server/database/providers/cidades/index.ts
@@ -0,0 +1,14 @@
+import * as create from './Create';
+//import * as getAll from './GetAll';
+//import * as deleteById from './DeleteById';
+//import * as getById from './GetById';
+//import * as updateById from './UpdateById';
+
+
+export const cidadesProvider = {
+ ...create,
+ //...getAll,
+ //...deleteById,
+ //...getById,
+ //...updateById
+};
